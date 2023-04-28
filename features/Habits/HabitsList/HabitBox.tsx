a77e73fa7fdc86d514b1486d15d70ad26a76d53b
Nikola Gospodinov <ngospodinov@scalable.com>
2023-04-28 09:03:29 +0100

    small style fix

--- features/Habits/HabitsList/HabitBox.tsx
+++ features/Habits/HabitsList/HabitBox.tsx
@@ -1,36 +1,26 @@
 import Link from 'next/link'
 
 import { Habit } from '../habits.types'
-import style from '../habit.module.scss'
 import {
     getCurrentStreak,
     getHabitCompletionIcon,
-    isHabitCompleted,
-    isHabitCompletedToday
+    isHabitCompleted
 } from '../helpers'
+
+import style from '../habit.module.scss'
 
 export const HabitBox: React.FunctionComponent<{
     habit: Habit
 }> = ({ habit }) => {
-    const {
-        name,
-        description,
-        target,
-        metric,
-        type,
-        urlPath,
-        currentStreak,
-        progress
-    } = habit
+    const { name, description, target, metric, type, urlPath, currentStreak } =
+        habit
     const href = `/habits/${urlPath}`
     const streak = getCurrentStreak({
         lastCompleted: currentStreak.end,
         currentStreak: currentStreak.streak,
         type
     })
-    const completedToday = isHabitCompleted(habit)
-    const weeklyTargetCompleted = (progress?.progress ?? 0) >= target
-    const completed = completedToday || weeklyTargetCompleted
+    const completed = isHabitCompleted(habit)
     const Icon = getHabitCompletionIcon(completed)
     const boxStyle = completed
         ? style.habitBoxCompleted
