a0d51e519b689d016b4962ff039f3b9048a08f50
Nik Gospodinov <nikola96gospodinov@gmail.com>
2023-05-22 16:36:38 +0100

    changing the color of the goal box border based on the goal status

--- features/Goal/GoalBox.tsx
+++ features/Goal/GoalBox.tsx
@@ -6,48 +6,61 @@ import { Deadline } from '../Deadline'
 
 export const GoalBox: React.FunctionComponent<{
     goal: Goal
-}> = ({ goal }) => (
-    <Link
-        href={`/goals/${goal.urlPath}`}
-        bg='white'
-        color='purple.600'
-        p={6}
-        borderRadius='2xl'
-        transition='0.2s ease'
-        position='relative'
-        boxShadow='inset'
-        fontWeight={500}
-        fontSize='md'
-        borderLeft='solid'
-        borderLeftColor='purple.600'
-        borderLeftWidth={3}
-        _hover={{
-            bg: 'white',
-            transform: 'translateY(4px)'
-        }}
-    >
-        <VStack
-            color='neutral.900'
-            align='flex-start'
-            justify='space-between'
-            h='100%'
-        >
-            <Box>
-                <Heading
-                    as='h3'
-                    fontWeight={600}
-                    fontSize='xl'
-                    lineHeight='1.35'
-                    mb={2}
-                >
-                    {goal.name}
-                </Heading>
-                <Text>{goal.description}</Text>
-            </Box>
+}> = ({ goal }) => {
+    const borderColor = (() => {
+        switch (goal.status) {
+            case 'active':
+                return 'purple.600'
+            case 'archived':
+                return 'neutral.400'
+            case 'completed':
+                return 'green.500'
+        }
+    })()
 
-            <Box pt={goal.description ? 2 : 4}>
-                <Deadline deadline={goal.deadline} gap={1.5} />
-            </Box>
-        </VStack>
-    </Link>
-)
+    return (
+        <Link
+            href={`/goals/${goal.urlPath}`}
+            bg='white'
+            color='purple.600'
+            p={6}
+            borderRadius='2xl'
+            transition='0.2s ease'
+            position='relative'
+            boxShadow='inset'
+            fontWeight={500}
+            fontSize='md'
+            borderLeft='solid'
+            borderLeftColor={borderColor}
+            borderLeftWidth={3}
+            _hover={{
+                bg: 'white',
+                transform: 'translateY(4px)'
+            }}
+        >
+            <VStack
+                color='neutral.900'
+                align='flex-start'
+                justify='space-between'
+                h='100%'
+            >
+                <Box>
+                    <Heading
+                        as='h3'
+                        fontWeight={600}
+                        fontSize='xl'
+                        lineHeight='1.35'
+                        mb={2}
+                    >
+                        {goal.name}
+                    </Heading>
+                    <Text>{goal.description}</Text>
+                </Box>
+
+                <Box pt={goal.description ? 2 : 4}>
+                    <Deadline deadline={goal.deadline} gap={1.5} />
+                </Box>
+            </VStack>
+        </Link>
+    )
+}
